8cc1eb9a8281e2fcda95fb02fba574112e3b7b6b
sohanipayla <sohanipayla@gmail.com>
2026-04-06 18:16:13 +0530

    Refactor income categorization using numpy

--- CH3/PB192.py
+++ CH3/PB192.py
@@ -1,13 +1,7 @@
 import pandas as pd
-df = pd.DataFrame({
-    'income': [25000, 40000, 75000, 60000, 20000]
-})
-def categorize_income(income):
-    if income < 30000:
-        return 'Low'
-    elif income > 70000:
-        return 'High'
-    else:
-        return 'Medium'
-df['income_group'] = df['income'].apply(categorize_income)
+import numpy as np
+
+df['Income_Group'] = np.where(df['income'] < 30000, 'Low',
+                       np.where(df['income'] <= 70000, 'Medium', 'High'))
+
 print(df)
