76462df3b13fb9aed618c8e449282c1cf049d23e
sohanipayla <sohanipayla@gmail.com>
2026-04-08 08:24:33 +0530

    Update PB62.py

--- CH1/PB62.py
+++ CH1/PB62.py
@@ -1,23 +1,28 @@
 import pandas as pd
-df = pd.read_csv('ipl-matches.csv')
-df
+df = pd.read_csv("ipl-matches.csv")
+df.info()
 
 # 1. Matches in superover
-s=df[df['SuperOver'] == 'Y']
-print(s)
+super_over_matches = df[df["SuperOver"] == "Y"]
+print(super_over_matches)
 
 # 2. CSK wins at Kolkata
-csk=df[(df['WinningTeam'] == 'Chennai Super Kings') & (df['City'] == 'Kolkata')]
-print(csk.shape[0])
+csk_kolkata = df[(df["WinningTeam"] == "Chennai Super Kings") & (df["City"] == "Kolkata")]
+print(len(csk_kolkata))
 
 # 3. Dhoni POTM vs Mumbai Indians
-mi=df[(df['Player_of_Match'] == 'MS Dhoni') & ((df['Team1'] == 'Mumbai Indians') | (df['Team2'] == 'Mumbai Indians'))]
-print(mi.shape[0])
+dhoni_mi = df[(df["Player_of_Match"] == "MS Dhoni") & 
+              ((df["Team1"] == "Mumbai Indians") | (df["Team2"] == "Mumbai Indians"))]
+
+print(len(dhoni_mi))
 
 # 4. GT won toss, elected to bat, and won match
-gt=df[(df['TossWinner'] == 'Gujarat Titans') & (df['TossDecision'] == 'bat') & (df['WinningTeam'] == 'Gujarat Titans')]
-gt
+gt_matches = df[(df["TossWinner"] == "Gujarat Titans") &
+                (df["TossDecision"] == "bat") &
+                (df["WinningTeam"] == "Gujarat Titans")]
+
+print(gt_matches)
 
 # 5. All matches won by Gujarat Titans
-all_gt=df[df['WinningTeam'] == 'Gujarat Titans']
-all_gt
+gt_wins = df[df["WinningTeam"] == "Gujarat Titans"]
+print(gt_wins)
