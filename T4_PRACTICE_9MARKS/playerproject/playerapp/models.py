7bd731427044ffd1f674b7a8079ee3e27e8383af
sohanipayla <sohanipayla@gmail.com>
2026-07-02 12:22:05 +0530

    Delete T4_PRACTICE_9MARKS/playerproject directory

--- T4_PRACTICE_9MARKS/playerproject/playerapp/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Player(models.Model):
-    name = models.CharField(max_length=100)
-    test_innings = models.IntegerField()
-    runs = models.IntegerField()
-
-    def __str__(self):
-        return self.name
